151c5666909d070500b3b33dcbeefdc69aeeee8d
Tânia <106488757+meganPopy@users.noreply.github.com>
2024-08-07 23:43:34 -0300

    a

---
+++ main.js
@@ -0,0 +1,21 @@
+/*const primeiroNumero = 1; 
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = Math.round (primeiroNumero/numeroPontoFlutuante); 
+
+console.log(novaOperacao);
+
+console.log (Math.round (17.0454540));
+
+console.log (Math.round (17.4454540));*/
+
+var idade = 16;
+
+if (idade > 18) {
+    console.log("ele nao pode viajar sozinho");
+} else if (idade >= 15) {
+    console.log("ele só pode viajar sozinho com autorização");
+} else {
+    console.log("ele nao pode viajar");
+}
